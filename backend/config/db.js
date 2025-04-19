9b16589958b042e799c028774069baa2ab9de72b
Ganga Singh <gangasingh1734@gmail.com>
2025-04-20 01:17:26 +0530

    getting route defined

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const Dbconnect = async ()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI)
         console.log("The data base has been connected")
+        console.log(process.env.MONGO_URI)
     } catch (error) {
         console.log("There was an error connecting the database",error)
     }
