93e6407557e52f3477025d7724724231471afc5f
Ganga Singh <gangasingh1734@gmail.com>
2025-04-20 14:25:53 +0530

    Backend completed

--- backend/index.js
+++ backend/index.js
@@ -4,81 +4,14 @@ const dotenv = require("dotenv");
 const { productmiddleware, productdeletemidddleware } = require("./middleware/product.auth");
 const { CreateProduct } = require("./types/product.validation");
 const { Product } = require("./models/product.model");
+const productrouter = require("./routes/product.routes")
 
 const app = express();
 dotenv.config();
 
+app.use("/api",productrouter)
 app.use(express.json());
 
-app.post("/api/create",productmiddleware, async (req,res)=>{
-    const paylod = req.body;
-    const parsedpaylod = CreateProduct.safeParse(paylod);
-
-    if(!parsedpaylod.success){
-        res.status(200).json({
-            msg:"Please send the right inputs"
-        })
-        return
-    }
-    else{
-        try {
-            const newProduct = await Product.create({
-                name:paylod.name,
-                price:paylod.price,
-                image:paylod.image
-            })
-            console.log(newProduct)
-            res.status(200).json({
-                msg:"The Product Has been Created"                
-            })
-        } catch (error) {
-            console.log("There was an error creating the product",error )
-        }
-    }
-})
-
-app.put("/api/update/:id",productdeletemidddleware,async (req,res)=>{
-    const {id} = req.params;
-    const product = req.body
-
-    try {
-        await Product.findByIdAndUpdate(id,product,{new:true})
-        res.status(200).json({
-            msg:"The Product has been deleted"
-        })
-    } catch (error) {
-        res.status(403).json({
-            msg:"There was an error deleting the product"
-        })
-    }
-})
-
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json({
-            products: products
-        });
-    } catch (err) {
-        res.status(500).json({ error: "Failed to fetch products", details: err.message });
-    }
-});
-
-
-app.get("/api/delete/:id",productdeletemidddleware,async(req,res)=>{
-    const {id} = req.params;
-
-    try {
-        await Product.findByIdAndDelete(id) 
-        res.status(200).json({
-            msg:"The Product has been deleted"
-        })
-    } catch (error) {
-        res.status(401).json({
-            msg:"There was an error deleting tge product"
-        })
-    }
-})
 
 app.listen(3000,()=>{
     Dbconnect();
